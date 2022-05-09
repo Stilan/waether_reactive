3b20e69a7c3f5b7d85e73f5ab2bbd88b9c6b8fb1
Stilan <40999153+Stilan@users.noreply.github.com>
2022-05-10 02:22:17 +0700

    2. Spring boot Webflux [#328882]

---
+++ src/main/java/ru/job4j/weather/waether_reactive/WaetherReactiveApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.weather.waether_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaetherReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaetherReactiveApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/job4j/weather/waether_reactive/Weather.java
@@ -0,0 +1,29 @@
+package ru.job4j.weather.waether_reactive;
+
+public class Weather {
+    private int id;
+
+    private String city;
+
+    private int temperature;
+
+    public Weather(int id, String city, int temperature) {
+        this.id = id;
+        this.city = city;
+        this.temperature = temperature;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+
+}
